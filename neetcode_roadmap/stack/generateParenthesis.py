e0f692fda1dd12917bccc1e86899bb83cccc3be8
Lucas <lucasvw89@gmail.com>
2024-04-18 23:32:11 -0300

    solution that i used on generate parenthesis

--- neetcode_roadmap/stack/generateParenthesis.py
+++ neetcode_roadmap/stack/generateParenthesis.py
@@ -1,28 +1,15 @@
-# recursion
-res=[]
-def gen(n: int, i='(', j=1):
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        res=[]
+        def gen(n: int, i='(', j=1):
 
-    if len(i) == 2*n:
-        if j == 0: res.append(i)
-        return
+            if len(i) == 2*n:
+                if j == 0: res.append(i)
+                return
 
-    gen(n, i+'(', j+1)
-    if j > 0:
-        gen(n, i+')', j-1)
+            gen(n, i+'(', j+1)
+            if j > 0:
+                gen(n, i+')', j-1)
 
-
-
-# iteration
-def genParenthesis(n: int) -> list[str]:
-    stack: list[str] = list()
-    i: int = 0
-    j: int = 0
-
-    while True:
-        stack.append('(')
-        if j > 0: stack.append(')')
-
-
-
-gen(3)
-print(res)
+        gen(n)
+        return res
