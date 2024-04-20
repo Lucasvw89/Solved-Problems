4ea263b69d09f19d285215f9414836be1a3676f9
Lucas <lucasvw89@gmail.com>
2024-04-20 15:21:16 -0300

    better solution for trapping water

--- neetcode_roadmap/TwoPointers/TrappingRainWater.py
+++ neetcode_roadmap/TwoPointers/TrappingRainWater.py
@@ -1,6 +1,6 @@
 class Solution:
     # brute force solution (pretty bad)
-    def trap(self, height: list[int]) -> int:
+    def trap_bad(self, height: list[int]) -> int:
         result: int = 0
         i: int = 0
         j: int = 0
@@ -24,3 +24,27 @@ class Solution:
                 height[i] -= 1
         
         return result
+
+
+    # better O(n) solution
+    def trap(self, height: list[int]) -> int:
+            result: int = 0
+            i, j = 0, len(height) - 1
+            i_max, j_max = height[i], height[j]
+
+            while i < j:
+                if height[i] <= height[j]:
+                    i += 1
+                    if height[i] < i_max:
+                        result += i_max - height[i]
+                    else:
+                        i_max = height[i]
+                else:
+                    j -= 1
+                    if height[j] < j_max:
+                        result += j_max - height[j]
+                    else:
+                        j_max = height[j]
+            
+            return result
+
